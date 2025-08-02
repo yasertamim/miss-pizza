5ff0f4b5d6f2357d13e159f3d193dcebdfba03f5
yaser tamim <yaser.timim@frameworks.no>
2025-08-02 20:38:36 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,22 @@
+ const filterButtons = document.querySelectorAll('.opt');
+  const menuItems = document.querySelectorAll('.card-m');
+
+  filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const filter = button.getAttribute('data-filter');
+
+    menuItems.forEach(item => {
+    const category = item.getAttribute('data-category');
+    if (filter === 'all' || category === filter) {
+        item.classList.remove('hidden');
+    } else {
+        item.classList.add('hidden');
+    }
+    });
+
+
+      // Optional: update active button style
+      filterButtons.forEach(btn => btn.classList.remove('primary'));
+      button.classList.add('primary');
+    });
+  });
